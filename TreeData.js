ed7a2902c7bf6b7f3b603758494b5555f4419a05
gopalratakonda <39745478+gopalratakonda@users.noreply.github.com>
2023-06-14 13:54:26 +0530

    Add files via upload

---
+++ TreeData.js
@@ -0,0 +1,344 @@
+var treeData=
+{		
+	"name": "Adiah",
+	"Spouse": "No info",
+	"children": [
+	{
+		"name" : "Punnaiah",
+		"Spouse": "Venkata Narasamma",
+		"children": [
+					{
+				"name" : "Prakash Rao",
+				"Spose" : "Hymavathi",
+				"children" :[
+								{
+									"name" : "Sridevi",
+									"Spouse" : "Ravi Gutta",
+									"children" : [{"name" : "Reshma","Spouse" : "Praveen"}]
+									
+								},
+								{
+									"name" : "Punnaiah",
+									"Spouse": "Rama",
+									"children" :[
+													{"name" : "Aditya","Spouse" : "Single"},
+													{"name" : "Shannu","Spouse" : "Single"}
+												]
+									
+					
+								},
+								{
+									"name" : "Prem Chand",
+									"Spouse" : "Sharada",
+									"children" : [
+													{"name" : "Jenny","Spouse" : "Single"}									
+												 ]
+								}
+								
+							]
+					}		
+					]
+		
+	},
+	{
+		"name" : "Seshaiah",
+		"Spouse" : "Lakshmi Narasamma",
+		"children" : [
+						{
+							"name" : "Rajamalleswararao",
+							"Spouse" : "Suguna",
+							"children" :[
+											{
+												"name" : "Seshagiri",
+												"Spouse" : "Karunasri Malampati",
+												"children" : [
+																{"name" : "Pujitha","Spouse" : "Satish Gadde"},
+																{"name" : "Susmitha", "Spouse" : "Vasista Potluri"}																	
+															  ]
+											},
+											{
+												"name" : "Vijay Kumar",
+												"Spouse" : "Anitha",
+												"children" : [
+																{"name" : "Abhishiktha", "Spouse" : "Venkat Nagandla"},
+																{"name" : "Manikanta", "Spouse" : "Single"}
+															 ]
+											},
+											{
+												"name" : "Kavitha", 
+												"Spouse" : "Suresh Gutta"
+											}
+										]
+						},
+						{
+							"name" : "Narasimharao",
+							"Spouse" : "Suguna",
+							"children" : [
+											{
+												"name" : "Srinivas",
+												"Spouse" : "Vani",
+												"children" : [
+																{"name" : "Saranya" , "Spouse":"Anil Vallabaneni"},
+																{"name" : "Karunya", "Spouse" : ""}
+															 ]
+											},
+											{
+												"name" : "Ashok",
+												"Spouse" : "",
+												"children" : [{"name":"","Spouse":"Single"}]
+											}
+										 ]
+						},
+						{"name" : "Jansi", "Spouse" : ""},
+						{"name" : "Kasturi", "Spouse": ""},
+						{"name" : "Rajani", "Spouse": ""}
+					 ]
+	},
+	{
+		"name" : "Laksmi Narayara",	
+		"Spouse" : "",
+		"children" :[
+					  {
+						  "name": "Apparao",
+						  "Spouse" : "Anasurya",
+						  "children": [
+										{
+											"name" : "Srinivas",
+											"Spouse": "Padmaja",		
+											"children" :[
+															{"name" : "Nikila"}
+														]
+												
+										},
+										{
+											"name" : "Bagya Lakshmi",
+											"Spouse": ""
+										},
+										{
+											"name" : "Ratna mala",
+											"Spouse": ""
+										}
+									  ]
+					  },
+					  {
+						  "name": "Mallaiah",
+						  "Souse": "",		
+						  "children": [	
+										{
+											"name" : "Bhanu",
+											"Spouse" : ""
+										},
+										{
+											"name" : "Sujatha",
+											"Spouse" : ""											
+										}							  
+									  ]
+					  },
+					  {
+						  "name" : "Krishnamurthi",
+						  "Spouse" : "",
+						  "children": [
+										{"name" : "Srinu",
+										 "Spouse" : ""
+										}
+									  ]
+					  },
+					  {
+						  "name" : "Satyanarayana",
+						  "Spouse" : "Suguna",		
+						  "children" : [
+											{"name" : "Rajesh",
+											 "Spouse" : ""
+											},
+											{"name" : "Goutham",
+											 "Spouse" : ""
+											}
+									   ]
+							
+					  },
+					  {
+						  "name" : "Ankinidu",
+						  "Spouse" : "",
+						  "children" : [
+											{
+												"name" : "Bobby",
+												"Spouse" : ""
+											},
+											{
+												"name" : "Chinnu",
+												"Spouse" : ""
+											}
+										]
+					  }
+					]
+	},
+	{
+		"name": "Pattabhi Ramaiah",
+		"FirstName": "Pattabhi Ramaiah",
+		"Spouse": "Adi Lakshmi",
+		"Residant At" : "Malkapuram, AP",
+		"children": [			{
+				"name": "Koteswaramma",
+				"Spouse": "Kotaiah Gutta",
+				"Residant At" : "Malkapuram, AP"
+			},
+			{
+				"name": "Sita",
+				"Spouse": "Mohanrao Malampati",
+				"Residant At" : "Malkapuram, AP"
+			},
+			{
+				"name": "Punnaiah",
+				"Spouse": "Vimala",
+				"Residant At" : "Khammam, TS",
+				"children": [{
+						"name": "Avani",
+						"Spouse": "Sudhakar Mandalapu",
+					},
+					{
+						"name": "Praveen",
+						"Spouse": "Sridevi",
+						"children": [{
+								"name": "Akshitha",
+								"Spouse" : "Single"
+							},
+							{
+								"name": "Anvitha",
+								"Spouse" : "Single"
+							}
+						]
+					}
+				]
+			},
+			{
+				"name": "Venkateswararao",
+				"Spouse": "Sarala",
+				"Residant At" : "Hyderabad, TS",
+				"children": [{
+						"name": "Naveen",
+						"children": [{
+								"name": "Abhiram",
+								"Spouse" : "Single"
+							},
+							{
+								"name": "Arjun",
+								"Spouse" : "Single"
+							}
+						]
+					},
+					{
+						"name": "Gopal",
+						"FirstName": "Gopal",
+						"Spouse": "Chandana Gutta",
+						"Residant At" : "Boston, USA",
+						"children": [{
+							"name": "ishitha","Spouse" : "Single"
+						}]
+					}
+				]
+			}
+
+		]
+	},	
+	{
+		"name" : "Krishnaiah",
+		"Spouse" : "",
+		"children" : [
+						{
+							"name" : "Chandrasekarrao",
+							"Spouse" : ""							
+						},
+						{
+							"name" : "Ankinidu",
+							"Spouse" : ""
+						},
+						{
+							"name" : "Hariprasad",
+							"Spouse": ""
+						},
+						{
+							"name" : "Rupa",
+							"spouse" : ""
+						}
+					 ]
+	},
+	{
+		"name" : "Venkaiah",
+		"Spouse":"",
+		"children" : [
+						{
+							"name" : "Seshagiri",
+							"Spouse" : "",
+							"children" : [{"name" : "Purnima"}]
+						},
+						{
+							"name" : "Sambasivarao",
+							"Spouse" : ""
+						},
+						{
+							"name" : "Rajyam",
+							"Spouse" : ""
+						}
+					 ]
+		
+	},
+	{
+		"name" : "Nagamalleswararo",
+		"Spouse":"Rajeswari",
+		"children" : [
+						{
+							"name" : "Ankinidu",
+							"Spouse" : "",
+							"children" : [{"name" : "Pavani","Spouse":""},{"name":"Pallavi","Spouse":""}]
+						},
+						{
+							"name" : "Ashok",
+							"Spouse" : "Madhu",
+							"children" : [{"name":""},{"name":""}]
+						}
+					 ]
+		
+	},
+	{
+		"name": "Partha Saradhi Naidu",
+		"Spouse": "Annapurna",
+		"Residant At" : "Malkapuram, AP",
+		"children":[
+					{"name": "Renuka",
+					 "Spouse": "Viswam Uppaluri",},
+					{
+					 "name": "Vijay Kumar",
+					 "Spouse": "Latha",
+					 "children":[
+									{"name" : "Yamini","Spouse" : ""},
+									{"name" : "Madhuri","Spouse" : "Single"}
+					 
+								]
+						
+					},
+					{
+					  "name": "Srinivas",
+					  "Spouse": "Revathi",
+					  "children": [
+									{"name" : "Aditya","Spouse" : "Single"},
+									{"name" : "Anusha","Spouse" : "Single"}									
+								 ]
+					}
+				]
+	},
+	{
+		"name" : "Anasurya",
+		"Spouse" : "___ Gutta"
+	},
+	{
+		"name" : "Nagaratnamma",
+		"Spouse" : "_____ Ganapaneni"
+		
+	},
+	{
+		"name" : "Annapurnamma",
+		"Spouse" : "____ Bhagam"
+	}
+	
+	]
+}
